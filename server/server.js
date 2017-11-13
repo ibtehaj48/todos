97ae790e2c5e5b1c81b7e24d1da5f77aec5634a1
abul <abs.tarannum007@gmail.com>
2017-11-13 20:43:29 +0100

    first POST route adn refactor mongoose

--- server/server.js
+++ server/server.js
@@ -1,4 +1,41 @@
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/Todo');
-var {User} = require('./models/User');
+//Require Express and Body Parser
+var express = require('express');
+var bodyParser = require('body-parser');
 
+//Require the local modules and 
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+//middleware use
+app.use(bodyParser.json());
+
+//route for todos
+app.post('/todos',(req,res) => {
+
+	//display the post data
+	console.log('New POST req \n',req.body);
+
+	//create a new todo entry using the req.body
+	var todo = new Todo({
+		text : req.body.text
+	});
+
+	//save the data
+	todo.save().then((doc) => {
+		res.send(doc);
+	}, (e) => {
+		res.status(400).send(e);
+	});
+
+});
+
+
+
+
+//App starts listening
+app.listen(3000, () => {
+	console.log('Starting the App..');
+})
